f98f54c2f8ea9cd34b6313043a01460f794149d8
Vishnu Mandala <51233895+vishnumandala@users.noreply.github.com>
2025-03-10 20:48:32 -0400

    Update gdrive.ts to default

--- packages/formatters/src/gdrive.ts
+++ packages/formatters/src/gdrive.ts
@@ -11,73 +11,87 @@ export function gdriveFormat(
 } {
   let name: string = '';
 
-  if (!minimalistic && stream.provider) {
+  if (stream.provider) {
     const cacheStatus = stream.provider.cached
       ? '⚡'
       : stream.provider.cached === undefined
-      ? '❓'
-      : '⏳';
+        ? '❓'
+        : '⏳';
     const serviceShortName =
       serviceDetails.find((service) => service.id === stream.provider!.id)
         ?.shortName || stream.provider.id;
-    name += `[${serviceShortName}${cacheStatus}]\n`;
+    name += [${serviceShortName}${cacheStatus}]\n;
   }
 
-  if (!minimalistic && stream.torrent?.infoHash) {
-    name += `[P2P]\n`;
+  if (stream.torrent?.infoHash) {
+    name += [P2P]\n;
   }
 
+  name += ${stream.addon.name} ${stream.personal ? '(Your Media) ' : ''};
   if (!minimalistic) {
-    name += `${stream.addon.name} `;
+    name += stream.resolution;
+  } else {
+    name += stream.resolution !== 'Unknown' ? stream.resolution + '' : '';
   }
 
-  name += `${stream.personal ? '(Your Media) ' : ''}${stream.resolution}`;
-
+  // let description: string = ${stream.quality !== 'Unknown' ? '🎥 ' + stream.quality + ' ' : ''}${stream.encode !== 'Unknown' ? '🎞️ ' + stream.encode : ''};
   let description: string = '';
-
   if (stream.quality || stream.encode) {
-    description += stream.quality !== 'Unknown' ? `🎥 ${stream.quality} ` : '';
-    description += stream.encode !== 'Unknown' ? `🎞️ ${stream.encode} ` : '';
+    description += stream.quality !== 'Unknown' ? 🎥 ${stream.quality}  : '';
+    description += stream.encode !== 'Unknown' ? 🎞️ ${stream.encode}  : '';
     description += '\n';
   }
 
   if (stream.visualTags.length > 0 || stream.audioTags.length > 0) {
-    description += stream.visualTags.length > 0
-      ? `📺 ${stream.visualTags.join(' | ')}   `
-      : '';
-    description += stream.audioTags.length > 0
-      ? `🎧 ${stream.audioTags.join(' | ')}`
-      : '';
+    description +=
+      stream.visualTags.length > 0
+        ? 📺 ${stream.visualTags.join(' | ')}   
+        : '';
+    description +=
+      stream.audioTags.length > 0 ? 🎧 ${stream.audioTags.join(' | ')} : '';
     description += '\n';
   }
+  if (
+    stream.size ||
+    (stream.torrent?.seeders && !minimalistic) ||
+    (minimalistic && stream.torrent?.seeders && !stream.provider?.cached) ||
+    stream.usenet?.age ||
+    stream.duration
+  ) {
+    description += 📦 ${formatSize(stream.size || 0)} ;
+    description += stream.duration
+      ? ⏱️ ${formatDuration(stream.duration)} 
+      : '';
+    description +=
+      (stream.torrent?.seeders !== undefined && !minimalistic) ||
+      (minimalistic && stream.torrent?.seeders && !stream.provider?.cached)
+        ? 👥 ${stream.torrent.seeders} 
+        : '';
 
-  if (stream.size || (!minimalistic && stream.torrent?.seeders) || stream.duration) {
-    description += `📦 ${formatSize(stream.size || 0)} `;
-    description += stream.duration ? `⏱️ ${formatDuration(stream.duration)} ` : '';
-    description += !minimalistic && stream.torrent?.seeders ? `👥 ${stream.torrent.seeders} ` : '';
+    description += stream.usenet?.age ? 📅 ${stream.usenet.age}  : '';
+    description +=
+      stream.indexers && !minimalistic ? 🔍 ${stream.indexers} : '';
     description += '\n';
   }
 
   if (stream.languages.length !== 0) {
-    description += `🔊 ` + stream.languages.map(lang => {
-      const emoji = languageToEmoji(lang);
-      return `${lang} ${emoji ? emoji : ''}`;
-    }).join(' | ');
+    let languages = stream.languages;
+    if (minimalistic) {
+      languages = languages.map(
+        (language) => languageToEmoji(language) || language
+      );
+    }
+    description += 🔊 ${languages.join(' | ')};
     description += '\n';
   }
 
   if (!minimalistic && stream.filename) {
-    description += `📄 ${stream.filename}\n`;
+    description += stream.filename ? 📄 ${stream.filename} : '📄 Unknown';
+    description += '\n';
   }
-
-  if (!minimalistic && stream.indexers) {
-    description += `🔍 ${stream.indexers}\n`;
-  }
-
   if (stream.message) {
-    description += `📢 ${stream.message}`;
+    description += 📢 ${stream.message};
   }
-
   description = description.trim();
   name = name.trim();
   return { name, description };
