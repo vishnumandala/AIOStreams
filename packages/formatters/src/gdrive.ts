f209cafdc07ccfe5a866c0a817ef0be6ededda5e
Vishnu Mandala <51233895+vishnumandala@users.noreply.github.com>
2025-03-10 20:38:59 -0400

    Update gdrive.ts
    For minimalist theme

--- packages/formatters/src/gdrive.ts
+++ packages/formatters/src/gdrive.ts
@@ -11,31 +11,30 @@ export function gdriveFormat(
 } {
   let name: string = '';
 
-  if (stream.provider) {
+  if (!minimalistic && stream.provider) {
     const cacheStatus = stream.provider.cached
       ? '⚡'
       : stream.provider.cached === undefined
-        ? '❓'
-        : '⏳';
+      ? '❓'
+      : '⏳';
     const serviceShortName =
       serviceDetails.find((service) => service.id === stream.provider!.id)
         ?.shortName || stream.provider.id;
     name += `[${serviceShortName}${cacheStatus}]\n`;
   }
 
-  if (stream.torrent?.infoHash) {
+  if (!minimalistic && stream.torrent?.infoHash) {
     name += `[P2P]\n`;
   }
 
-  name += `${stream.addon.name} ${stream.personal ? '(Your Media) ' : ''}`;
   if (!minimalistic) {
-    name += stream.resolution;
-  } else {
-    name += stream.resolution !== 'Unknown' ? stream.resolution + '' : '';
+    name += `${stream.addon.name} `;
   }
 
-  // let description: string = `${stream.quality !== 'Unknown' ? '🎥 ' + stream.quality + ' ' : ''}${stream.encode !== 'Unknown' ? '🎞️ ' + stream.encode : ''}`;
+  name += `${stream.personal ? '(Your Media) ' : ''}${stream.resolution}`;
+
   let description: string = '';
+
   if (stream.quality || stream.encode) {
     description += stream.quality !== 'Unknown' ? `🎥 ${stream.quality} ` : '';
     description += stream.encode !== 'Unknown' ? `🎞️ ${stream.encode} ` : '';
@@ -43,55 +42,42 @@ export function gdriveFormat(
   }
 
   if (stream.visualTags.length > 0 || stream.audioTags.length > 0) {
-    description +=
-      stream.visualTags.length > 0
-        ? `📺 ${stream.visualTags.join(' | ')}   `
-        : '';
-    description +=
-      stream.audioTags.length > 0 ? `🎧 ${stream.audioTags.join(' | ')}` : '';
+    description += stream.visualTags.length > 0
+      ? `📺 ${stream.visualTags.join(' | ')}   `
+      : '';
+    description += stream.audioTags.length > 0
+      ? `🎧 ${stream.audioTags.join(' | ')}`
+      : '';
     description += '\n';
   }
-  if (
-    stream.size ||
-    (stream.torrent?.seeders && !minimalistic) ||
-    (minimalistic && stream.torrent?.seeders && !stream.provider?.cached) ||
-    stream.usenet?.age ||
-    stream.duration
-  ) {
-    description += `📦 ${formatSize(stream.size || 0)} `;
-    description += stream.duration
-      ? `⏱️ ${formatDuration(stream.duration)} `
-      : '';
-    description +=
-      (stream.torrent?.seeders !== undefined && !minimalistic) ||
-      (minimalistic && stream.torrent?.seeders && !stream.provider?.cached)
-        ? `👥 ${stream.torrent.seeders} `
-        : '';
 
-    description += stream.usenet?.age ? `📅 ${stream.usenet.age} ` : '';
-    description +=
-      stream.indexers && !minimalistic ? `🔍 ${stream.indexers}` : '';
+  if (stream.size || (!minimalistic && stream.torrent?.seeders) || stream.duration) {
+    description += `📦 ${formatSize(stream.size || 0)} `;
+    description += stream.duration ? `⏱️ ${formatDuration(stream.duration)} ` : '';
+    description += !minimalistic && stream.torrent?.seeders ? `👥 ${stream.torrent.seeders} ` : '';
     description += '\n';
   }
 
   if (stream.languages.length !== 0) {
-    let languages = stream.languages;
-    if (minimalistic) {
-      languages = languages.map(
-        (language) => languageToEmoji(language) || language
-      );
-    }
-    description += `🔊 ${languages.join(' | ')}`;
+    description += `🔊 ` + stream.languages.map(lang => {
+      const emoji = languageToEmoji(lang);
+      return `${lang} ${emoji ? emoji : ''}`;
+    }).join(' | ');
     description += '\n';
   }
 
   if (!minimalistic && stream.filename) {
-    description += stream.filename ? `📄 ${stream.filename}` : '📄 Unknown';
-    description += '\n';
+    description += `📄 ${stream.filename}\n`;
   }
+
+  if (!minimalistic && stream.indexers) {
+    description += `🔍 ${stream.indexers}\n`;
+  }
+
   if (stream.message) {
     description += `📢 ${stream.message}`;
   }
+
   description = description.trim();
   name = name.trim();
   return { name, description };
